18b876031f19ed41a08e1a969391f9a264c5bd65
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 17:59:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
 			:pieces => 4,
 			:names => {
 				:reading_railroad => {
-					
+
 				},
 				:pennsylvania_railroad => {
 
@@ -52,7 +52,10 @@ def monopoly_with_third_tier
 				}
 			},
 			:rent_in_dollars => {
-
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200
 			}
 		}
 	}
